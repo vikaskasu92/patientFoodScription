0f0b165b9b1c152500e56fdf3b2cf88ca26e71f0
vikaskasu92 <vikaskasu92@gmail.com>
2021-03-16 19:18:55 -0500

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SafariViewController } from '@ionic-native/safari-view-controller/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor(private safariViewController: SafariViewController) {}
+  ngOnInit(): void {
+    this.safariViewController.isAvailable().then((available: boolean) => {
+      if (available) {
+          this.safariViewController.show({
+            url: 'https://ionic.io',
+            hidden: false,
+            animated: false,
+            transition: 'curl',
+            enterReaderModeIfAvailable: true,
+            tintColor: '#ff0000'
+          })
+          .subscribe((result: any) => {
+              if(result.event === 'opened') console.log('Opened');
+              else if(result.event === 'loaded') console.log('Loaded');
+              else if(result.event === 'closed') console.log('Closed');
+            },
+            (error: any) => console.error(error)
+          );
+
+        } else {
+          // use fallback browser, example InAppBrowser
+          console.log("safari view controller is not available")
+        }
+      }
+    );
+  }
+
+  
 
 }
